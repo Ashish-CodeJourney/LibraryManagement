5c2fb06fd722939bab05b6be76f5a7a5ac81f8c4
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-09 22:53:17 +0530

    Refactor : Remove redundant class-level variables

--- app/src/test/java/LibraryManagement/LibraryManagementTest.java
+++ app/src/test/java/LibraryManagement/LibraryManagementTest.java
@@ -12,12 +12,6 @@ import org.junit.jupiter.api.Test;
 
 public class LibraryManagementTest {
 
-    // Dummy data of a book for Testing
-    private String isbn = "123456789";
-    private String title = "Clean Code";
-    private String author = "Robert C. Martin";
-    private int publicationYear = 2008;
-
     private LibraryManagement library;
 
     @BeforeEach
@@ -27,12 +21,8 @@ public class LibraryManagementTest {
 
     @Test
     void Test_Adding_Book () {
-        String isbn = "123456789";
-        String title = "Clean Code";
-        String author = "Robert C. Martin";
-        int publicationYear = 2008;
 
-        Book book = new Book(isbn, title, author, publicationYear);
+        Book book = new Book("123456789", "Clean Code", "Robert C. Martin", 2008);
 
         library.addBook(book);
 
@@ -56,12 +46,8 @@ public class LibraryManagementTest {
 
     @Test
     void Test_Borrowing_Book () {
-        String isbn = "123456789";
-        String title = "Clean Code";
-        String author = "Robert C. Martin";
-        int publicationYear = 2008;
 
-        Book book = new Book(isbn, title, author, publicationYear);
+        Book book = new Book("123456789", "Clean Code", "Robert C. Martin", 2008);
 
         // Addding the book first
         library.addBook(book);
@@ -88,12 +74,8 @@ public class LibraryManagementTest {
 
     @Test
     void Test_Returning_Book () {
-        String isbn = "123456789";
-        String title = "Clean Code";
-        String author = "Robert C. Martin";
-        int publicationYear = 2008;
 
-        Book book = new Book(isbn, title, author, publicationYear);
+        Book book = new Book("123456789", "Clean Code", "Robert C. Martin", 2008);
 
         // Addding the book first
         library.addBook(book);
