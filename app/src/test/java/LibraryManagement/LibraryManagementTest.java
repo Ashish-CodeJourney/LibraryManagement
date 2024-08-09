6c55c2a1fcb9db2b00363480891699830010f167
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-09 21:44:34 +0530

    Add setUp method for every testcase using @beforeeach annotation for LibraryManagement

--- app/src/test/java/LibraryManagement/LibraryManagementTest.java
+++ app/src/test/java/LibraryManagement/LibraryManagementTest.java
@@ -1,4 +1,20 @@
 package LibraryManagement;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LibraryManagementTest {
+
+    // Dummy data of a book for Testing
+    private String isbn = "123456789";
+    private String title = "Clean Code";
+    private String author = "Robert C. Martin";
+    private int publicationYear = 2008;
+
+    private LibraryManagement library;
+
+    @BeforeEach
+    void setUp() {
+        library = new LibraryManagement();
+    }
 }
