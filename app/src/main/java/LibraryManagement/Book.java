62f575417c89fde13540a03d38bc98e5b1ecd95f
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-09 21:35:25 +0530

    Implementation of BookCreation via constructor

--- app/src/main/java/LibraryManagement/Book.java
+++ app/src/main/java/LibraryManagement/Book.java
@@ -1,5 +1,38 @@
 package LibraryManagement;
 
 public class Book {
-    
+
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean isAvailable;
+
+    public Book (String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+         return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
