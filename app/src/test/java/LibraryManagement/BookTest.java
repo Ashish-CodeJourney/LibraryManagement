b32f32f9df7a53c44ce58f4274cc6080a4978669
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-09 21:32:17 +0530

    Add setUp method for every testcase using @beforeeach annotation

--- app/src/test/java/LibraryManagement/BookTest.java
+++ app/src/test/java/LibraryManagement/BookTest.java
@@ -1,4 +1,19 @@
 package LibraryManagement;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class BookTest {
+
+    // Dummy data of a book for Testing
+    private String isbn = "123456789";
+    private String title = "Clean Code";
+    private String author = "Robert C. Martin";
+    private int publicationYear = 2008;
+
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+        book = new Book(isbn, title, author, publicationYear);
+    }
 }
